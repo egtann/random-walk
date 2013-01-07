c530a827e0f3501b5d785cf85cbf8f46f3f5236b
Evan Tann <egtann@gmail.com>
2013-01-07 10:59:32 -0500

    Add generate_vectors and fit_y_to_x

--- lib/random-walk.rb
+++ lib/random-walk.rb
@@ -1,8 +1,9 @@
 class RandomWalk
+  attr_accessor :x, :y
+
   def self.generate(limits, array_length)
     starting_point = Random.rand(limits)
     output_array = [starting_point]
-
     (array_length - 1).times do
       add_or_subtract = Random.rand(-1..1)
       end_point = output_array.last + add_or_subtract
@@ -12,7 +13,24 @@ class RandomWalk
         output_array << output_array.last
       end
     end
-
     return output_array
+  end
+
+  def generate_vectors(x_limits, y_limits, vector_length)
+    self.x = RandomWalk.generate(x_limits, vector_length)
+    self.y = RandomWalk.generate(y_limits, vector_length)
+  end
+
+  def fit_y_to_x(fit_chance)
+    raise 'Fit chance must be between 0 and 100' unless fit_chance <= 100 && fit_chance >= 0
+    self.x.each_with_index do |value, i|
+      if self.x[i] && self.x[i+1]
+        x_diff = self.x[i] - self.x[i+1]
+        rand = Random.rand(100 / fit_chance)
+        if rand == 0
+          self.y[i+1] = self.y[i] - x_diff
+        end
+      end
+    end
   end
 end
--- spec/random-walk_spec.rb
+++ spec/random-walk_spec.rb
@@ -1,17 +1,62 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "RandomWalk" do
-  it "returns results between given limits" do
-    5.times do
-      results = RandomWalk.generate(0..10, 1000)
-      results.each do |result|
-        (result.should be >= 0) && (result.should be <= 10)
+  describe "generate" do
+    it "returns results between given limits" do
+      5.times do
+        results = RandomWalk.generate(0..10, 1000)
+        results.each do |result|
+          result.should be >= 0
+          result.should be <= 10
+        end
       end
+    end
+
+    it "returns an array of the correct length" do
+      results = RandomWalk.generate(100..200, 250)
+      results.length.should == 250
     end
   end
 
-  it "returns an array of the correct length" do
-    results = RandomWalk.generate(100..200, 250)
-    results.length.should == 250
+  describe "generate_vectors" do
+    let(:rw) { RandomWalk.new }
+
+    it "returns results between given limits" do
+      5.times do
+        rw.generate_vectors(0..10, 0..10, 1000)
+        rw.x.each do |result|
+          result.should be >= 0
+          result.should be <= 10
+        end
+        rw.y.each do |result|
+          result.should be >= 0
+          result.should be <= 10
+        end
+      end
+    end
+
+    it "returns 2 arrays of the correct length" do
+      rw.generate_vectors(0..99, 0..1023, 500)
+      rw.x.length.should == 500
+      rw.y.length.should == 500
+    end
+  end
+
+  describe "fit_y_to_x" do
+    let(:rw) { RandomWalk.new }
+
+    it "fits the y array to the x array perfectly" do
+      rw.generate_vectors(0..99, 250..1000, 500)
+      fit_chance = 100
+      rw.fit_y_to_x(fit_chance)
+      diff_x, diff_y = 0, 0
+      rw.x.each_with_index do |result, i|
+        if rw.x[i+1]
+          diff_x += rw.x[i] - rw.x[i+1]
+          diff_y += rw.y[i] - rw.y[i+1]
+        end
+      end
+      diff_x.should == diff_y
+    end
   end
 end
